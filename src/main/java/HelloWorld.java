f8f279b2261032977bd13ad65e9532907a7b019b
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2021-11-15 15:40:33 +0100

    my changes

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");
+        System.out.println("Marcin");
+        System.out.println("44");
+        System.out.println("K");// [3]
+
+    }
+}
