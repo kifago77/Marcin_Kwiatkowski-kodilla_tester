93231aa0c1977d11681120f7b1965551886fcff1
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2021-11-16 10:16:49 +0100

    my changes

---
+++ src/main/java/main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args)  {
+        String myName = "Marcin";
+        int myAge = 44;
+        double height = 1.80;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+
+
+
+    }
+}
