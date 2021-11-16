1ce2f74c05134bc4f73a406289ec01b339a2e704
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2021-11-16 10:59:54 +0100

    my changes

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended{
+    public static void main(String[] args) {
+        String CarName = "Ferrari";
+        System.out.println(CarName);
+        int model = 365;
+        System.out.println(model);
+        double engine = 4.8;
+        System.out.println(engine);
+        char carClass = 'F';
+        System.out.println(carClass);
+        boolean isNotGood = false;
+        System.out.println(isNotGood);
+        System.out.println(CarName);
+
+
+    }
+}
