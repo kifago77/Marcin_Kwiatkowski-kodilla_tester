f497af412d76830177836e95579f31d4ae9e8e0e
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2021-12-02 16:58:54 +0100

    my changes

--- src/main/java/DebugExample.java
+++ src/main/java/DebugExample.java
@@ -1,13 +1,29 @@
 public class DebugExample {
     public static void main(String[] args) {
-        int firstNumber = 11;
+        int firstNumber = 30;
         int secondNumber = 22;
 
         System.out.println(firstNumber);
         System.out.println(secondNumber);
         sumAndDisplay.sumAndDisplay(firstNumber, secondNumber);
-        System.out.println(sumAndDisplay.sum(23, 11));
+        System.out.println(sumAndDisplay.sum(30, 22));
+        if (firstNumber > secondNumber) {
+            subtractAndDisplay(firstNumber, secondNumber);
+        } else {
+            sumAndDisplay(firstNumber, secondNumber);
+        }
 
     }
+    private static void sumAndDisplay(int a, int b) {
+        int result = a + b;
+        System.out.println(result);
+    }
+
+    private static void subtractAndDisplay(int a, int b) {
+        int result = a - b;
+        System.out.println(result);
+    }
+
 }
+
 
--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -7,6 +7,10 @@ public class FirstClass {                           // [1]
         System.out.println(text);
         System.out.println(text + Example);
 
-        // [3]
+        if (1 > 2) {
+            System.out.println("Niesamowite! Jeden jest większe niż dwa");
+        } else {
+            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa");
+        }
     }
 }
