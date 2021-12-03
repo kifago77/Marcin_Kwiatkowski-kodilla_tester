52db1c47b2041559e57401c0ce7956a1926119a3
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2021-12-03 20:09:52 +0100

    my changes

---
+++ src/main/java/Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    public static void main(String[] args) {
+        String[] names = new String[3];
+        names[0] = "Zygfryd";
+        names[1] = "Gwidon";
+        names[2] = "Florentyna";
+        System.out.println(names.length);
+        for (int i = 0; i < names.length; i++)
+            System.out.println(names[i]);
+    }
+}
