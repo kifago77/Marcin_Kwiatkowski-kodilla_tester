2b9fe46aac7b0bc3df13ffb53a5c58be49aebdcc
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2021-12-03 19:14:39 +0100

    my changes

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -3,8 +3,8 @@ public class LeapYear {
         return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
     }
     public static void main(String[] args) {
-        boolean resalt = leap(2021);
-        System.out.println(resalt);
+        boolean result = leap(2020);
+        System.out.println(result);
 
     }
 }
---
+++ src/main/java/SimpleArray.java
@@ -0,0 +1,12 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] animal = new String[5];
+        animal[0] = "dog";
+        animal[1] = "cat";
+        animal[2] = "birds";
+        animal[3] = "horse";
+        animal[4] = "fish";
+        System.out.println(animal[3]);
+    }
+
+}
