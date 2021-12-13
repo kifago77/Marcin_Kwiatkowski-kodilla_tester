bb5e43a1fd38b7461c67e82faa6cf35ffd2e3de6
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2021-12-13 21:17:36 +0100

    my changes

--- src/main/java/Loops.java
+++ src/main/java/Loops.java
@@ -1,11 +1,31 @@
 public class Loops {
     public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++)
+            System.out.println(i);
         String[] names = new String[3];
+        int numberElements = names.length;
         names[0] = "Zygfryd";
         names[1] = "Gwidon";
         names[2] = "Florentyna";
+        System.out.println(names[2]);
         System.out.println(names.length);
-        for (int i = 0; i < names.length; i++)
+        for (int i = names.length - 1; i >= 0; i-- ) {
             System.out.println(names[i]);
+        }
+        int i = 0;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+
+
     }
-}
+
+
+    }
+
+
+
+
+
+
