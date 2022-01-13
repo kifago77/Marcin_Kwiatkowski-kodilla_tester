20258416c413cd31f239e4c518e0a7fa901f0978
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2022-01-13 19:11:42 +0100

    my changes

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,34 +1,31 @@
 public class FirstClass {                           // [1]
     public static void main(String[] args) {         // [2]
-        System.out.println("Hello from FirstClass!");
-        String Example = "Hello from FirstClass";
-        System.out.println(Example);
-        String text = (Example);
-        System.out.println(text);
-        System.out.println(text + Example);
-
-        if (1 > 2) {
-            System.out.println("Niesamowite! Jeden jest większe niż dwa");
-        } else {
-            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa");
-        }
-
 
         Notebook notebook = new Notebook("600g", 1000);
+        System.out.println(notebook.weight);
+        System.out.println(notebook.price);
         System.out.println(notebook.weight + notebook.price);
+        System.out.println(notebook.weight + " " + notebook.price);
+        if (notebook.price < 900) {
+            System.out.println("This notebook is cheap.");
+        } else {
+            System.out.println("This notebook is quite expensive.");
+        }
         notebook.checkPrice();
 
 
-        Notebook heavyNotebook = new Notebook("2000g", 1500);
+        Notebook  heavyNotebook =  new Notebook("2000g", 1500);
+        System.out.println(heavyNotebook.weight);
+        System.out.println(heavyNotebook.price);
         System.out.println(heavyNotebook.weight + heavyNotebook.price);
         heavyNotebook.checkPrice();
 
-        Notebook oldNotebook = new Notebook("1200g",500);
-        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+
+        Notebook oldNotebook = new Notebook("1600g", 500);
+        System.out.println(oldNotebook.weight);
+        System.out.println(oldNotebook.price);
+        System.out.println(oldNotebook.weight + oldNotebook.price);
         oldNotebook.checkPrice();
-
-
-
 
     }
 }
--- src/main/java/Notebook.java
+++ src/main/java/Notebook.java
@@ -1,22 +1,19 @@
 public class Notebook {
     String weight;
     int price;
-    public Notebook(String weight, int price) {
+    public Notebook(String weight,int price) {
         this.weight = weight;
         this.price = price;
 
     }
     public void checkPrice() {
-        if (this.price < 600) {
-            System.out.println("This notebook is very cheep.");
+        if (this.price < 900) {
+            System.out.println("This notebook is cheap.");
+        } else {
+            System.out.println("This notebook is quite expensive.");
+        }
     }
-        if (this.price < 800) {
-            System.out.println("The price is good");
         }
 
-        if (this.price > 1000) {
-            System.out.println("This notebook is expensive.");
-        }
 
-        }
-}
+
