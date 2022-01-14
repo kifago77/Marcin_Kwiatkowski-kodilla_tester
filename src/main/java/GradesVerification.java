f619b57e85a6d0bfdceb364533f848c7fbc26036
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2022-01-14 19:22:01 +0100

    my changes

--- src/main/java/GradesVerification.java
+++ src/main/java/GradesVerification.java
@@ -1,5 +1,7 @@
 public class GradesVerification {
     public static void main(String[] args) {
-        Grades grades = new Grades();
+        Grades grades = new Grades(); {
+            System.out.println(grades);
+        }
     }
 }
