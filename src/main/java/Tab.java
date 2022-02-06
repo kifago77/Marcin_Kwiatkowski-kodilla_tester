6fd3c22d4d9c738118cc2a08f4379e1ac2964040
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2022-02-06 15:10:59 +0100

    my changes

---
+++ src/main/java/Tab.java
@@ -0,0 +1,29 @@
+public class Tab {
+    public static void main(String[] args) {
+
+        //Tworzenie tablicy dwuwymiarowej o parametrach 3x5
+        int[][] tab = new int[2][5];
+        tab[0][0] = 10;
+        tab[0][1]= 5;
+        tab[0][2]= 8;
+        tab[0][3]= 123;
+        tab[0][4]= 23;
+        System.out.println(tab[0][1]);
+        tab[1][0]= 12;
+        tab[1][1] = 15;
+        tab[1][2] = 134;
+        tab[1][3] = 19;
+        tab[1][4] = 90;
+        System.out.println(tab[1][4]);
+        System.out.println(tab.length);
+        int sum = tab[0][2] + tab[1][0];
+        System.out.println(sum);
+
+
+
+
+
+    }
+
+
+}
--- src/main/java/Tablica.java
+++ src/main/java/Tablica.java
@@ -1,19 +1,18 @@
 public class Tablica {
     public static void main(String[] args) {
         String[] movie = new String[]{"Harry", "Potter", "Gra", "Oś Zła"};
-        System.out.println();
-
         System.out.println(movie.length);
 
         String[] film = new String[3];
-        film[0] = "Harry";
-        film[1] = "Potter";
+        film[0] = "Harry Potter";
+        film[1] = "Sun";
         film[2] = "Gra";
 
         System.out.println(film[2]);
 
         String text = "Tablica zawiera trzy filmy";
         System.out.println(text);
+        System.out.println("Tab.zaw.3 filmy:" + film[0] + film[1] + film[2]);
 
         int numberFilm = film.length;
         System.out.println(numberFilm);
