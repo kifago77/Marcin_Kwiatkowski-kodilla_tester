d27b3ba9ff6e81f259a01ca7f63737474822e453
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2022-03-14 21:44:22 +0100

    my changes

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,5 +1,7 @@
 package com.kodilla.basic_assertion;
 
+
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,5 +14,29 @@ public class CalculatorTestSuite {
         int b = 8;
         int sumResult = calculator.sumNumber(a, b);
         assertEquals(13, sumResult);
+    }
+    @Test
+    public void testSub(){
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int subResult = calculator.subtract(a, b);
+        assertEquals(-3, subResult);
+    }
+    @Test
+    public  void testSquare(){
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int squResult = calculator.square(a);
+        assertEquals(25, squResult);
+
+    }
+    @Test
+    public void testDivision(){
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int diviResult = calculator.division(a, b);
+        assertEquals(40, diviResult);
     }
 }
---
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/PrimeCheckerTestSuite.java
@@ -0,0 +1,7 @@
+package com.kodilla.basic_assertion;
+
+public class PrimeCheckerTestSuite {
+    public boolean isPrime(int number){
+        return true;
+    }
+}
