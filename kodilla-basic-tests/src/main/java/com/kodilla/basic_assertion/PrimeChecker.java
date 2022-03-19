deffcfeda8bd8e22b2c478d898397d296760d420
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2022-03-19 11:01:58 +0100

    my changes

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/PrimeChecker.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/PrimeChecker.java
@@ -1,7 +1,19 @@
 package com.kodilla.basic_assertion;
 
-public class PrimeChecker {
-    public boolean isPrime(int number){
+public class PrimeChecker{
+    public boolean isPrime(int number) {
         return true;
-    }
 }
+
+
+}
+
+
+
+
+
+
+
+
+
+
