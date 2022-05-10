6e9e7e96a18c4f453b30baedca757e7f91b2dba8
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2022-05-10 12:12:16 +0200

    My changes

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/ShapeApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/ShapeApplication.java
@@ -7,30 +7,30 @@ import com.kodilla.collections.interfaces.Triangle;
 
 import java.util.Random;
 
-public class ShapeApplication {
-    public static void main(String[] args) {
+    public class ShapeApplication {
 
+        public static void main(String[] args) {
+            Shape[] shapes = new Shape[5];
+            for (int n = 0; n < shapes.length; n++)
+                shapes[n] = drawShape();
+            for (Shape shape : shapes)
+                ShapeUtils.displayShapeInfo(shape);
+        }
+
+        private static Shape drawShape() {
+            Random random = new Random();
+            int drawnShapeKind = random.nextInt(3);
+            double a = random.nextDouble() * 100 + 1;
+            double b = random.nextDouble() * 100 + 1;
+            double c = random.nextDouble() * 100 + 1;
+            if (drawnShapeKind == 0)
+                return new Circle();
+            else if (drawnShapeKind == 1)
+                return new Square(a);
+            else
+                return new Triangle(a, b, c);
+        }
     }
 
-    private static Shape drawnShape(){
-        Random random = new Random();
-        int drawnShapeKid = random.nextInt(3);
-        double a = random.nextDouble() * 100 + 1;
-        double b = random.nextDouble() * 100 + 1;
-        double c = random.nextDouble() * 100 + 1;
-        return new Shape() {
-            @Override
-            public double getArea() {
-                return 0;
-            }
-
-            @Override
-            public double getPerimeter() {
-                return 0;
-            }
-        };
 
 
-    }
-
-    }
