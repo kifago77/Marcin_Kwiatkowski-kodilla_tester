b936af56bbe529077a1a3ef33b87ccc24bba451c
Marcin Kwiatkowski <mf.kwiatkowski@wp.pl>
2022-05-10 12:06:28 +0200

    My changes

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/ShapeApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/ShapeApplication.java
@@ -1,4 +1,36 @@
 package com.kodilla.collections.arrays;
 
+import com.kodilla.collections.interfaces.Circle;
+import com.kodilla.collections.interfaces.Shape;
+import com.kodilla.collections.interfaces.Square;
+import com.kodilla.collections.interfaces.Triangle;
+
+import java.util.Random;
+
 public class ShapeApplication {
-}
+    public static void main(String[] args) {
+
+    }
+
+    private static Shape drawnShape(){
+        Random random = new Random();
+        int drawnShapeKid = random.nextInt(3);
+        double a = random.nextDouble() * 100 + 1;
+        double b = random.nextDouble() * 100 + 1;
+        double c = random.nextDouble() * 100 + 1;
+        return new Shape() {
+            @Override
+            public double getArea() {
+                return 0;
+            }
+
+            @Override
+            public double getPerimeter() {
+                return 0;
+            }
+        };
+
+
+    }
+
+    }
